e3448bc98c6408825dbdc0d0ab589f9ecc48d46b
Schmeka <113141846+Schmeka@users.noreply.github.com>
2026-08-18 11:44:41 -0400

    went from angle control to torque control

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,14 @@ float angle_x = 0.0f;
 float angle_y = 0.0f;
 float angle_z = 0.0f;
 
+//Control parameters
+float Kp_ang_x = 3.0f;
+float Kp_vel_x = 0.2f;
+float Ki_vel_x = 0.01f;
+float Kd_vel_x = 0.0001f; // DESTINY 2 REFERENCE!
+float vel_integral_x = 0.0f;
+float prev_vel_error_x = 0.0f;
+
 void initAS5048A() {
     pinMode(PA4, OUTPUT);
     digitalWrite(PA4, HIGH);
@@ -43,9 +51,9 @@ float readAS5048A() {
     return ((float)raw_angle / 16384.0f) * _2PI; 
 }
 
-GenericSensor encoder = GenericSensor(readAS5048A, initAS5048A);
-BLDCDriver3PWM driver = BLDCDriver3PWM(PA0, PA1, PA2);
-BLDCMotor motor = BLDCMotor(11);
+GenericSensor encoder_x = GenericSensor(readAS5048A, initAS5048A);
+BLDCDriver3PWM driver_x = BLDCDriver3PWM(PA0, PA1, PA2);
+BLDCMotor motor_x = BLDCMotor(11);
 
 void initMPU6050() { 
     Wire.setSDA(PB7);
@@ -131,45 +139,34 @@ void setup() {
     calibrate();
 
     // Initialize AS5048A Encoder
-    encoder.init();
-    motor.linkSensor(&encoder);
+    encoder_x.init();
+    motor_x.linkSensor(&encoder_x);
 
     // Initialize DRV8313 Driver
-    driver.voltage_power_supply = 12;
-    driver.pwm_frequency = 32000;
-    driver.init();
-    motor.linkDriver(&driver);
+    driver_x.voltage_power_supply = 12;
+    driver_x.pwm_frequency = 32000;
+    driver_x.init();
+    motor_x.linkDriver(&driver_x);
     
     // Motor settings
-    motor.controller = MotionControlType::angle;
-    motor.torque_controller = TorqueControlType::voltage;
-    motor.foc_modulation = FOCModulationType::Trapezoid_120;
-    motor.voltage_limit = 6.0f;
+    motor_x.controller = MotionControlType::torque;
+    motor_x.torque_controller = TorqueControlType::voltage;
+    motor_x.foc_modulation = FOCModulationType::Trapezoid_120;
+    motor_x.voltage_limit = 6.0f;
 
-    motor.PID_velocity.P = 0.16f;
-    motor.PID_velocity.I = 0.001f;
-    motor.PID_velocity.D = 0.0001f; // DESTINY 2 REFERENCE!
-    motor.LPF_velocity.Tf = 0.01f;
-    motor.PID_velocity.output_ramp = 50.0f;
-    motor.PID_velocity.limit = 2.0f;
-    // motor.motion_downsample = 10;
 
-    motor.P_angle.P = 0.05f;
-    motor.P_angle.I = 0.001f;
-    motor.P_angle.D = 0.0f;
-
-    motor.init();
-    motor.initFOC();
+    motor_x.init();
+    motor_x.initFOC();
     
-    motor.P_angle.reset();
-    motor.PID_velocity.reset();
+    motor_x.P_angle.reset();
+    motor_x.PID_velocity.reset();
 
     prev_time = micros();
 }
 
 void loop() {
     // 1. FOC Update
-    motor.loopFOC();
+    motor_x.loopFOC();
     
     // 2. MPU Math
     unsigned long now = micros();
@@ -224,21 +221,24 @@ void loop() {
         last_print = millis();
     }
     
-    // 2. Calculate the target voltage based on the IMU tilt
-    // (Assuming you want to balance around the X-axis. Change theta_x to theta_y if needed)
-    float target_angle = 0.0f; 
-    float error = target_angle - theta_x;
+    float theta_x_rad = theta_x * (PI / 180.0f);
+    float gx_rads = gx_dps * (PI / 180.0f);
 
-    // float target_voltage = motor.PID_velocity(target_angle);
+    float target_angle_rad_x = 0.0f; 
+    float angle_error_x = target_angle_rad_x - theta_x_rad;
+    float target_velocity_x = Kp_ang_x * angle_error_x;
 
-    // Serial.print("Error: "); Serial.println(error, 3);
-    
-    // // 3. Constrain the voltage for safety so it doesn't exceed your 6V limit
-    // if (target_voltage > 6.0f) 
-    //     target_voltage = 6.0f;
-    // if (target_voltage < -6.0f) 
-    //     target_voltage = -6.0f;
-    
-    // 3. Send the calculated voltage to the motor
-    motor.move(error);
+    float velocity_error_x = target_velocity_x - gx_rads;
+
+
+    vel_integral_x += velocity_error_x * dt;
+    vel_integral_x = constrain(vel_integral_x, -motor_x.voltage_limit, motor_x.voltage_limit); 
+
+    float derivative_x = (velocity_error_x - prev_vel_error_x) / dt;
+    prev_vel_error_x = velocity_error_x;
+
+    float target_voltage_x = (Kp_vel_x * velocity_error_x) + (Ki_vel_x * vel_integral_x) + (Kd_vel_x * derivative_x);
+    target_voltage_x = constrain(target_voltage_x, -motor_x.voltage_limit, motor_x.voltage_limit);
+
+    motor_x.move(target_voltage_x);
 }
